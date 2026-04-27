57193ce9ba17d31f7555c19c0c95540336e262a3
Seraphim <Nihaz.444.s.d@mail.ru>
2026-04-27 16:09:17 +0300

    исправил роут

--- app/api/v1/health.py
+++ app/api/v1/health.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from app.schemas.health_schema import HealthSchema
 from datetime import datetime
 
 router = APIRouter(prefix="/health")
+
 @router.get("/", response_model=HealthSchema)
-async def health(status: str = "ok", time: datetime = datetime.now(), message: str | None = None):
-        return {"status": status, "time": time, "message": message}
+async def health(
+        status: str = Query(default="ok", description="Статус"),
+        message: str | None = Query(default=None, description="Доп сообщение")
+):
+        return {
+                "status":status,
+                "message":message,
+                "timestamp" : datetime.now(),
+        }
